6ad1f7d2bd321d74391e3f144e43064c50051db1
vineetls <vineet@mudupalli.com>
2017-03-26 02:52:02 -0700

    fixed articles
    bumpy feels with all text

--- xml_link_extractor.py
+++ xml_link_extractor.py
@@ -1,7 +1,7 @@
 import rssparser
 import numpy
 opened = open('incrediblerss.xml', 'r')
-# opened = open('crediblerss.xml', 'r')
+#opened = open('crediblerss.xml', 'r')
 
 str = opened.read()
 strarr = []
@@ -9,7 +9,12 @@ strarr = []
 while(str.find('<link>') != -1):
     url = str[str.find("<link>")+6: str.find("</link>",str.find("<link>"))]
     if(url.find('rssmix') == -1):
-        strarr.append (url )
+        print(url)
+        try:
+            strarr.append (rssparser.get_text_from_url(url) )
+        except:
+            pass
+        print(strarr)
     str = str[str.find("</link>")+1:]
 print(strarr)
 numpy.save('incrediblerss',strarr)
