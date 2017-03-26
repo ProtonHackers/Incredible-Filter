844a9f41c975eee6c8a603029e0b579ff64bb7fa
Vineetls <vineet@mudupalli.com>
2017-03-26 01:51:56 -0700

    Twitter search (#19)
    * Twitter integration
    
    Integration and validation of fake news sites.
    
    * fixes merge conflicts
    
    * rss and xml link extraction done
    
    rss feeds extracted + code on how to do it
    
    * Fix Merge Conflicts

--- xml_link_extractor.py
+++ xml_link_extractor.py
@@ -1,12 +1,16 @@
-# opened = open('incrediblerss.xml', 'r')
-opened = open('crediblerss.xml', 'r')
+import rssparser
+import numpy
+opened = open('incrediblerss.xml', 'r')
+# opened = open('crediblerss.xml', 'r')
+
 str = opened.read()
-newstr = ''
+strarr = []
 
 while(str.find('<link>') != -1):
     url = str[str.find("<link>")+6: str.find("</link>",str.find("<link>"))]
     if(url.find('rssmix') == -1):
-        newstr += (url +"\n")
+        strarr.append (url )
     str = str[str.find("</link>")+1:]
+print(strarr)
+numpy.save('incrediblerss',strarr)
 
-print(newstr)
