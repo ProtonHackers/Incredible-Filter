136352b405bf1e31f7c31c6e3f473ece6c8b000e
RK900 <rovik05@gmail.com>
2017-03-26 02:03:32 -0700

    Add more data to antibully

--- predict_cyber.py
+++ predict_cyber.py
@@ -11,6 +11,6 @@ from antibully import iterate_tone_analysis, get_features
 
 pickle.load(open(os.getcwd() + '/svm-cyber.p','rb'))
 
-new = get_features(iterate_tone_analysis('what a tim'))
+new = get_features(iterate_tone_analysis('viks i want to kill you'))
 print svc.predict(new)
 
